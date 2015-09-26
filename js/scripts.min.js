2ff78da0f7d60ec04a715e19ff65fb50902c9ea5
Nico Watine <contact@studioncreations.com>
2015-09-26 13:50:09 +0200

    featured-gallery.php DONE & some JS Organized

--- js/scripts.min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".slider-for").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,asNavFor:".slider-nav"}),$(".slider-nav").slick({slidesToShow:3,slidesToScroll:1,asNavFor:".slider-for",centerMode:!0,centerPadding:"30px",focusOnSelect:!0}),$(".slides").slick()});
